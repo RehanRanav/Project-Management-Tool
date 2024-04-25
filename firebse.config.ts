89de6964559f7b8fd465d0db5ca95f3f2e53d416
RehanRanav <155937908+RehanRanav@users.noreply.github.com>
2024-04-25 15:19:10 +0530

    Update and rename firebse.config.example.ts to firebse.config.ts
    change for deployement

--- firebse.config.ts
+++ firebse.config.ts
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY as string,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN as string,
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
+  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET as string,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID as string,
   appId: process.env.FIREBASE_APP_ID as string,
